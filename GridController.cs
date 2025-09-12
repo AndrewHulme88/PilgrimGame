6805401cc8d265972c0cb1f77b12eb4236d89f5d
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-09-12 17:45:26 +1000

    Add files via upload

--- GridController.cs
+++ GridController.cs
@@ -6,6 +6,8 @@ public class GridController : MonoBehaviour
     public Transform maxPoint;
     public GrowBlock growBlockPrefab;
 
+    private Vector2Int gridSize;
+
     void Start()
     {
         GenerateGrid();
@@ -23,6 +25,18 @@ public class GridController : MonoBehaviour
 
         Vector3 startPoint = minPoint.position + new Vector3(0.5f, 0.5f, 0f);
 
-        Instantiate(growBlockPrefab, startPoint, Quaternion.identity);
+        //Instantiate(growBlockPrefab, startPoint, Quaternion.identity);
+
+        gridSize = new Vector2Int(Mathf.RoundToInt(maxPoint.position.x - minPoint.position.x), Mathf.RoundToInt(maxPoint.position.y - minPoint.position.y));
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                GrowBlock newBlock = Instantiate(growBlockPrefab, startPoint + new Vector3(x, y, 0f), Quaternion.identity);
+
+                newBlock.transform.SetParent(transform);
+            }
+        }
     }
 }
