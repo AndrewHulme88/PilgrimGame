40cb3726273da699ed5dab4c52441a64fe488233
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-09-22 12:26:13 +1000

    Add files via upload

--- ShopController.cs
+++ ShopController.cs
@@ -1,12 +1,23 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
+    public ShopSeedDisplay[] shopSeedDisplays;
+
     public void OpenClose()
     {
         if(UIController.instance.inventoryController.gameObject.activeSelf == false)
         {
-            gameObject.SetActive(!gameObject.activeSelf);        
+            gameObject.SetActive(!gameObject.activeSelf);       
+            
+            if(gameObject.activeSelf == true)
+            {
+                foreach (ShopSeedDisplay shopSeedDisplay in shopSeedDisplays)
+                {
+                    shopSeedDisplay.UpdateDisplay();
+                }
+            }
         }
     }
 }
---
+++ ShopSeedDisplay.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static CropController;
+
+public class ShopSeedDisplay : MonoBehaviour
+{
+    public CropController.CropType seedType;
+    public Image seedImage;
+    public TMP_Text seedAmountText;
+    public TMP_Text seedCostText;
+
+    public void UpdateDisplay()
+    {
+        CropInfo cropInfo = CropController.instance.GetCropInfo(seedType);
+        seedImage.sprite = cropInfo.seedType;
+        seedAmountText.text = "x" + cropInfo.seedAmount;
+        seedCostText.text = "$" + cropInfo.seedPrice + " each";
+    }
+
+    public void BuySeed(int amount)
+    {
+
+    }
+}
