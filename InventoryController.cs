2c9d817da176d82b5b67c82bb0302cde59d66ba0
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-09-22 10:03:00 +1000

    Add files via upload

--- InventoryController.cs
+++ InventoryController.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    
+    public SeedDisplay[] seedDisplays;
+
     public void OpenClose()
     {
         if(gameObject.activeSelf == false)
         {
             gameObject.SetActive(true);
+            UpdateSeedDisplays();
         }
         else
         {
             gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdateSeedDisplays()
+    {
+        foreach (SeedDisplay seedDisplay in seedDisplays)
+        {
+            seedDisplay.UpdateDisplay();
         }
     }
 }
